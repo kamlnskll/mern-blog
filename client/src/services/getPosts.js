f17ff776b5760238b14affad704fecd0b467fb44
Michael <michael_kaminski@outlook.com>
2022-07-16 12:41:34 -0500

    messing around with axios fetch, aint workin

--- client/src/services/getPosts.js
+++ client/src/services/getPosts.js
@@ -1,16 +1,30 @@
 import axios from 'axios'
+import { useState } from 'react'
 
-export async function fetchPosts() {
+export default async function fetchPosts() {
   // Fetch post data
 
   try {
-    const response = await axios({
-      method: 'GET',
-      url: 'http://localhost:8080/api/posts',
-      data: {},
-    })
-    return response.data
+    const getPosts = await axios
+      .get('http://localhost:8080/api/posts')
+      .then((res) => {
+        console.log(res.data)
+      })
+    return (
+      <div class='card card-compact w-96 bg-base-100 shadow-xl'>
+        <figure>
+          <img src='https://placeimg.com/400/225/arch' alt='Shoes' />
+        </figure>
+        <div class='card-body'>
+          <h2 class='card-title'>Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class='card-actions justify-end'>
+            <button class='btn btn-primary'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+    )
   } catch (err) {
-    throw err
+    console.log(err)
   }
 }
