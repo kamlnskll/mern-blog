82b9519f3395a26248f5271c499d44c4fd1b46ed
Michael <michael_kaminski@outlook.com>
2022-07-22 19:28:05 -0500

    trying to figure out props

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Post = ({ id, setId }) => {
-  const [postData, setPostData] = useState(null)
+const Post = () => {
+  const [postData, setPostData] = useState('')
+  const [id, setId] = useState('')
 
   async function getPost() {
     await axios
-      .get('http://localhost:8080/api/posts/:id')
+      .get(`http://localhost:8080/api/posts/`)
       .then((res) => {
         console.log(res.data)
         setPostData(res.data)
+        setId(postData._id)
+        console.log(id)
       })
       .catch((err) => {
         console.log(err)
@@ -17,10 +20,11 @@ const Post = ({ id, setId }) => {
   }
 
   useEffect(() => {
-    getPost()
+    getPost(id)
+    console.log(id)
   }, [])
 
-  return <div>Here is the post page</div>
+  return <div>Here is the post page for post ID: ${} </div>
 }
 
 export default Post
