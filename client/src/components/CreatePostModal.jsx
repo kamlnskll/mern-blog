ba09e502e9c717b5b0f93e8636a7f61927e96043
Michael Kaminski <michael_kaminski@outlook.com>
2022-08-07 21:03:10 -0500

    updated create post modal and created form

--- client/src/components/CreatePostModal.jsx
+++ client/src/components/CreatePostModal.jsx
@@ -8,10 +8,31 @@ const CreatePostModal = () => {
 
 <input type="checkbox" id="my-modal-5" class="modal-toggle" />
 <div class="modal">
-  <div class="modal-box w-11/12 max-w-4xl">
-    <h1 class="font-extrabold text-2xl">Create a new post</h1>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-    <div className="modal-action border flex justify-center gap-4">
+  <div class="modal-box w-9/12 max-w-4xl">
+
+  <div>
+  <div>
+  <h1 className='text-3xl uppercase font-bold mt-4'>Create new post</h1>
+  </div>
+
+<div className='w-2/3 mx-auto pt-12'>
+  
+
+  <div alt="form" className='p-4'> 
+  <input type="text" placeholder="Post Title" class="input w-full input-primary" />
+  </div>
+
+  <div alt="form" className='p-4'> 
+  <textarea class="textarea textarea-bordered w-full" placeholder="Enter a short description of the post"></textarea>  
+  </div>
+
+<div alt="form" className='px-4'> 
+  <textarea class="textarea textarea-bordered w-full h-72" placeholder="Post body goes here"></textarea>  
+  </div>
+  </div>
+</div>
+
+    <div className="modal-action flex justify-center gap-2">
       <label for="my-modal-5" class="btn btn-warning">Add Post</label>
       <label for="my-modal-5" class="btn btn-primary">Cancel</label>
     
